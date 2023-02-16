624ae8ece10b96ba4ee68b3a8dd6aae9c3bc23f9
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-02-16 15:09:10 -0300

    test: ensure RemoteAuthentication calls HttpPostClint with correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,7 @@
 import { HttpPostClientSpy } from '../../protocols/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 import { faker } from '@faker-js/faker'
+import { mockAuthentication } from '../../../domain/test/mock-authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -20,7 +21,13 @@ describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with corret URL', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthentication())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+  test('Should call HttpPostClient with corret body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const autheticationParams = mockAuthentication()
+    await sut.auth(autheticationParams)
+    expect(httpPostClientSpy.body).toEqual(autheticationParams)
   })
 })
