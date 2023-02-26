3819c13607dcc8d95294e5c2b6a7a5f68a74b30a
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-02-26 18:26:02 -0300

    test: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { AxiosHttpClient } from './axios-http-client'
+import { faker } from '@faker-js/faker'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with correct URL', async () => {
+    const sut = new AxiosHttpClient()
+    const url = faker.internet.url()
+    await sut.post({ url })
+    expect(mockedAxios).toHaveBeenCalledWith(url)
+  })
+})
