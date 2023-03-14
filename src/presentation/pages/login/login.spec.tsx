8efad211856c1606f5935dcfee824776169f8045
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-03-14 18:48:32 -0300

    refactor: move AuthenticationSpy to its own file

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,21 +1,8 @@
 import React from 'react'
 import { render, type RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Login from './login'
-import { ValidationStub } from '@/presentation/test/'
+import { ValidationStub, AuthenticationSpy } from '@/presentation/test/'
 import { faker } from '@faker-js/faker'
-import { type Authentication, type AuthenticationParams } from '@/domain/usecases'
-import { type AccountModel } from '@/domain/models'
-import { mockAccountModel } from '@/domain/test'
-
-class AuthenticationSpy implements Authentication {
-  account = mockAccountModel()
-  params: AuthenticationParams
-
-  async auth (params: AuthenticationParams): Promise<AccountModel> {
-    this.params = params
-    return await Promise.resolve(this.account)
-  }
-}
 
 type SutTypes = {
   sut: RenderResult
---
+++ src/presentation/test/mock-authentication.ts
@@ -0,0 +1,13 @@
+import { type Authentication, type AuthenticationParams } from '@/domain/usecases'
+import { type AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/test'
+
+export class AuthenticationSpy implements Authentication {
+  account = mockAccountModel()
+  params: AuthenticationParams
+
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
+    this.params = params
+    return await Promise.resolve(this.account)
+  }
+}
