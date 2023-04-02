dc32da2dbf8ae5d346b499bdc3d030b541d915f8
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-02 11:18:22 -0300

    fix: change webpack env var to use JSON.stringify

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,10 @@ module.exports = {
     }]
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
+    static: './public',
+    devMiddleware: {
+      writeToDisk: true
+    },
     historyApiFallback: true
   },
   externals: {
@@ -47,7 +49,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new DefinePlugin({
-      'process.env.API_URL': 'http://localhost:5050/api'
+      'process.env.API_URL': JSON.stringify('http://localhost:5050/api')
     })
   ]
 }
