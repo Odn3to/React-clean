d7d0fe8955f3f31abb38c35687805e552aed3713
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-04 09:08:12 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct body

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -2,7 +2,7 @@ import { HttpPostClientSpy } from '@/data/test'
 import { type AccountModel } from '@/domain/models'
 import { type AddAccountParams } from '@/domain/usecases'
 import { RemoteAddAccount } from './remote-add-account'
-import { mockAddAccountParams } from '@/domain/test'
+import { mockAddAccountParams } from '@/domain/test/mock-add-account'
 import { faker } from '@faker-js/faker'
 
 type SutTypes = {
@@ -25,5 +25,12 @@ describe('RemoteAuthentication', () => {
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.add(mockAddAccountParams())
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Should call HttpPostClient with corret body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const addAccountParams = mockAddAccountParams()
+    await sut.add(addAccountParams)
+    expect(httpPostClientSpy.body).toEqual(addAccountParams)
   })
 })
