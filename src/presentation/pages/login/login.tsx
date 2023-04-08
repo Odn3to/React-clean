b15be27eaa39fb5ab645f055ebb74d9a80b234a4
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-08 15:18:45 -0300

    feat: ensure SignUp shows spinner on submit

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Styles from './login-styles.scss'
