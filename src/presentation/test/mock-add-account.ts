ae5a0cf0edf73cadcfd74dbc96676b192e38e9d5
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-08 15:46:39 -0300

    test: ensure SignUp calls AddAccount with correct values

---
+++ src/presentation/test/mock-add-account.ts
@@ -0,0 +1,14 @@
+import { type AddAccountParams, type AddAccount } from '@/domain/usecases'
+import { type AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/test'
+
+export class AddAccountSpy implements AddAccount {
+  account = mockAccountModel()
+  params: AddAccountParams
+  callsCount = 0
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    this.params = params
+    return this.account
+  }
+}
