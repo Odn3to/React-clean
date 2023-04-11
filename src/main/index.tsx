2cfb369a870205fc91faea46209b8b3ddce58ec7
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-11 15:55:10 -0300

    feat: add Signup COmposition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom'
 import Router from '@/presentation/components/router/router'
 import '@/presentation/styles/global.scss'
 import { makeLogin } from './factories/pages/login/login-factory'
+import { makeSignUp } from './factories/pages/signup/signup-factory'
 
 ReactDOM.render(
     <Router
         MakeLogin={makeLogin}
+        MakeSignUp={makeSignUp}
     />,
     document.getElementById('main')
 )
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,17 +1,17 @@
-import { Signup } from '@/presentation/pages'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-type Props = {
+type Factory = {
   MakeLogin: React.FC
+  MakeSignUp: React.FC
 }
 
-const Router: React.FC<Props> = ({ MakeLogin }: Props) => {
+const Router: React.FC<Factory> = (factory: Factory) => {
   return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<MakeLogin />} />
-                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<factory.MakeLogin />} />
+                <Route path="/signup" element={<factory.MakeSignUp />} />
             </Routes>
         </BrowserRouter>
   )
