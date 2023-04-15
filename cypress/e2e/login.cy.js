18b1b0f9f7e156c01435d601924ff2536a03f045
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-15 19:29:00 -0300

    test: ensure login present error message if invalid credentials are provided and valid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,6 +2,8 @@
 
 const { faker } = require("@faker-js/faker")
 
+const baseURL = 'http://localhost:8080/'
+
 Cypress.on('uncaught:exception', (err, runnable) => {
   // we expect a Tauri error about the window
   // and don't want to fail the test so we return false
@@ -14,7 +16,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('login', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080/login')
+    cy.visit(baseURL + 'login')
   })
   it('Should load with correct initial state', () => {
     cy.getByTestId('email').should('have.attr', 'readOnly')
@@ -54,5 +56,29 @@ describe('login', () => {
       .should('contain.text', '🟢')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+      .getByTestId('main-error').should('contain.text', 'Credenciais inválidos')
+    cy.url().should('eq', `${baseURL}login`)
+  })
+
+  it('Should present save accessToken if valid credentials are provided', () => {
+    cy.getByTestId('email').focus().type('odneto8812@gmail.com')
+    cy.getByTestId('password').focus().type('88126695')
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner').should('exist')
+      .getByTestId('main-error').should('not.exist')
+      .getByTestId('spinner').should('not.exist')
+    cy.url().should('eq', `${baseURL}`)
+    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
 })
