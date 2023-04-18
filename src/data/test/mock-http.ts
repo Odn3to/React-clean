fe6f1ea62e85d6bcb37a03a3c5ebab475c0560a6
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-18 19:46:21 -0300

    feat: ensure RemoteLoadSurveyList

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,10 +1,15 @@
 import { faker } from '@faker-js/faker'
 import { type HttpPostClient, type HttpPostParams, HttpStatusCode, type HttpResponse , type HttpGetClient, type HttpGetParams } from '@/data/protocols/http'
 
-export class HttpGetClientSpy implements HttpGetClient {
+export class HttpGetClientSpy<R> implements HttpGetClient <R> {
   url: string
-  async get (params: HttpGetParams): Promise<void> {
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  }
+
+  async get (params: HttpGetParams): Promise<HttpResponse<R>> {
     this.url = params.url
+    return this.response
   }
 }
 
