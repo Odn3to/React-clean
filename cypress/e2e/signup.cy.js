ee57299dd284e177b66dcdda73f3c26776d05d13
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-18 10:15:03 -0300

    test: ensure Signup prevent submit if form is invalid

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -85,4 +85,10 @@ describe('Signup', () => {
         cy.getByTestId('submit').dblclick()
         cy.get('@request.all').should('have.length', 1)
     })
+
+    it('Should not call submit is form is invalid', () => {
+        cy.mockOk(/signup/, { accessToken: faker.random.alphaNumeric() }).as('request')
+        cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+        cy.get('@request.all').should('have.length', 0)
+    })
 })
