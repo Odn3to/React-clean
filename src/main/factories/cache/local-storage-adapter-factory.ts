cfd165a93827a7ad87b4fe0f422c1303355a5fe9
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-04-27 11:40:44 -0300

    feat: add getCurrentAccountAdapter

--- src/main/factories/cache/local-storage-adapter-factory.ts
+++ src/main/factories/cache/local-storage-adapter-factory.ts
@@ -1,6 +1,5 @@
-import { LocalStorageAdapter } from './../../../infra/cache/local-storage-adapter'
-import { type SetStorage } from '@/data/protocols/cache/set-storage'
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
-export const makeLocalStorageAdapter = (): SetStorage => {
+export const makeLocalStorageAdapter = (): LocalStorageAdapter => {
   return new LocalStorageAdapter()
 }
