3e9a5c3ccbaf44e58fecebbbf4f074bdf367e1e8
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-10 09:26:31 -0300

    feat: ensure AuthorizeHttpGetClientDecorator not add token if GetStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -2,10 +2,14 @@ import { type GetStorage } from '@/data/protocols/cache'
 import { type HttpResponse, type HttpGetClient, type HttpGetParams } from '@/data/protocols/http'
 
 export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
-  constructor (private readonly getStorage: GetStorage) {}
+  constructor (
+    private readonly getStorage: GetStorage,
+    private readonly httpGetClient: HttpGetClient
+  ) {}
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
     this.getStorage.get('account')
+    this.httpGetClient.get(params)
     return null
   }
 }
