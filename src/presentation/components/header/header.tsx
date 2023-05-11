a32982f02430c5ea99c6e64df2d38eb5848b7f6b
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-11 09:39:50 -0300

    feat: ensure header renders username correctly

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Header: React.FC = () => {
   const history = useNavigate()
-  const { setCurrentAccount } = useContext(ApiContext)
+  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
   const logout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
     event.preventDefault()
     setCurrentAccount(undefined)
@@ -18,7 +18,7 @@ const Header: React.FC = () => {
             <div className={Styles.headerContent}>
                 <Logo />
                 <div className={Styles.LogoutWrap}>
-                    <span>Odilon</span>
+                    <span data-testid="username">{getCurrentAccount().name}</span>
                     <a data-testid="logout" href='#' onClick={logout}>Sair</a>
                 </div>
             </div>
