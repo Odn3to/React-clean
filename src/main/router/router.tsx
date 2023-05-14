fa12dbd8a743f9b73706d5610bdaef8cee9dd92b
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-14 17:43:06 -0300

    feat: add cdn to external dependencies

--- src/main/router/router.tsx
+++ src/main/router/router.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { makeLogin, makeSignUp, makeSurveyList } from '@/main/factories/pages'
+import { BrowserRouter as Browser, Routes, Route } from 'react-router-dom'
+import { makeLoginValidation, makeSignUpValidation } from '@/main/factories/pages'
 import { ApiContext } from '@/presentation/contexts'
 import { setCurrentAccountAdapter, getCurrentAccountAdapter } from '../adapters/current-account-adapter'
 import PrivateRoute from '@/presentation/components/private-route/private-route'
+import { Login, Signup, SurveyList } from '@/presentation/pages'
+import { makeRemoteAddAccount, makeRemoteAuthentication, makeRemoteLoadSurveyList } from '../factories/usecases'
 
 const Router: React.FC = () => {
   return (
@@ -13,15 +15,29 @@ const Router: React.FC = () => {
             getCurrentAccount: getCurrentAccountAdapter
           }}
         >
-          <BrowserRouter>
+          <Browser>
               <Routes>
-                  <Route path="/login" Component={makeLogin} />
-                  <Route path="/signup" Component={makeSignUp} />
+                  <Route path="/login" element={
+                    <Login
+                      authentication={makeRemoteAuthentication()}
+                      validation={makeLoginValidation()}
+                    />}
+                  />
+                  <Route path="/signup" element={
+                    <Signup
+                      addAccount={makeRemoteAddAccount()}
+                      validation={makeSignUpValidation()}
+                    />}
+                    />
                   <Route element={<PrivateRoute />}>
-                    <Route path="/" Component={makeSurveyList} />
+                    <Route path="/" element={
+                      <SurveyList
+                        loadSurveyList={makeRemoteLoadSurveyList()}
+                      />}
+                    />
                   </Route>
               </Routes>
-          </BrowserRouter>
+          </Browser>
         </ApiContext.Provider>
   )
 }
