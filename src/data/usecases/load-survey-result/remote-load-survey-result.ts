32f1483ad47ae5a2672a75c76107a266e1a7e99f
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-15 22:37:03 -0300

    feat: ensure RemoteLoadSurveyResult returns SurveyResultModel on 200

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -1,18 +1,33 @@
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { type HttpGetClient } from './../../protocols/http/http-get-client'
 import { HttpStatusCode } from '@/data/protocols/http'
-export class RemoteLoadSurveyResult {
+import { type LoadSurveyResult } from '@/domain/usecases'
+export class RemoteLoadSurveyResult implements LoadSurveyResult {
   constructor (
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<RemoteLoadSurveyResult.Model>
   ) {}
 
-  async load (): Promise <void> {
+  async load (): Promise <LoadSurveyResult.Model> {
     const httpRespose = await this.httpGetClient.get({ url: this.url })
+    const remoteSurveyResult = httpRespose.body
     switch (httpRespose.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return Object.assign({}, remoteSurveyResult, { date: new Date(remoteSurveyResult.date) })
       case HttpStatusCode.forbiden: throw new AccessDeniedError()
       default: throw new UnexpectedError()
     }
+  }
+}
+
+export namespace RemoteLoadSurveyResult {
+  export type Model = {
+    question: string
+    date: string
+    answers: Array<{
+      image?: string
+      answer: string
+      count: number
+      percent: number
+    }>
   }
 }
