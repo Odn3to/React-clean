73da628020cc9343173ba969f952965ca5b84839
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-19 18:58:17 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,70 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Styles from './survey-result-styles.scss'
 import { Calendar, Footer, Header, Loading } from '@/presentation/components'
 import FlipMove from 'react-flip-move'
 
 const SurveyResult: React.FC = () => {
-  const [answers, setAnswers] = useState([{
-    image: 'http://fordevs.herokuapp.com/static/img/logo-react.png',
-    answer: 'ReactJs',
-    percent: 50,
-    isCurrentAccountAnswer: true
-  }, {
-    image: 'http://fordevs.herokuapp.com/static/img/logo-vue.png',
-    answer: 'VueJs',
-    percent: 60,
-    isCurrentAccountAnswer: false
-  },{
-    image: 'http://fordevs.herokuapp.com/static/img/logo-angular.png',
-    answer: 'Angular',
-    percent: 70,
-    isCurrentAccountAnswer: true
-  }
-  ])
-
   return (
         <div className={Styles.SurveyResultWrap}>
             <Header />
             <div className={Styles.contentWrap}>
-                <hgroup>
-                  <Calendar date={new Date()} className={Styles.calendarWrap} />
-                  <h2>Qual é seu framework web favorito?</h2>
-                </hgroup>
-                <FlipMove className={Styles.answerList}>
-                    {answers.map(a =>
-                    <li key={a.answer}>
-                        <img src={a.image}/>
-                        <span className={Styles.answer}>{a.answer}</span>
-                        <span className={Styles.percent}>{a.percent}</span>
-                    </li>
-                    )}
-                </FlipMove>
-                <button onClick={() => {
-                  setAnswers([
-                    {
-                      image: 'http://fordevs.herokuapp.com/static/img/logo-react.png',
-                      answer: 'ReactJs',
-                      percent: 52,
-                      isCurrentAccountAnswer: true
-                    }, {
-                      image: 'http://fordevs.herokuapp.com/static/img/logo-vue.png',
-                      answer: 'VueJs',
-                      percent: 61,
-                      isCurrentAccountAnswer: false
-                    },{
-                      image: 'http://fordevs.herokuapp.com/static/img/logo-angular.png',
-                      answer: 'Angular',
-                      percent: 76,
-                      isCurrentAccountAnswer: true
-                    }, {
-                      image: 'http://fordevs.herokuapp.com/static/img/logo-react.png',
-                      answer: 'ReactJs',
-                      percent: 60,
-                      isCurrentAccountAnswer: true
-                    }
-                  ])
-                }}>Voltar</button>
-                {false && <Loading />}
+              {false &&
+                <>
+                  <hgroup>
+                    <Calendar date={new Date()} className={Styles.calendarWrap} />
+                    <h2>Qual é seu framework web favorito?</h2>
+                  </hgroup>
+                  <FlipMove className={Styles.answerList}>
+                      <li>
+                          <img src='http://fordevs.herokuapp.com/static/img/logo-react.png'/>
+                          <span className={Styles.answer}>ReactJs</span>
+                          <span className={Styles.percent}>50%</span>
+                      </li>
+                      <li>
+                          <img src='http://fordevs.herokuapp.com/static/img/logo-vue.png'/>
+                          <span className={Styles.answer}>VueJs</span>
+                          <span className={Styles.percent}>60%</span>
+                      </li>
+                      <li>
+                          <img src='http://fordevs.herokuapp.com/static/img/logo-angular.png'/>
+                          <span className={Styles.answer}>Angular</span>
+                          <span className={Styles.percent}>70%</span>
+                      </li>
+                  </FlipMove>
+                  <button>Voltar</button>
+                </>
+              }
+              {false && <Loading />}
             </div>
             <Footer />
         </div>
