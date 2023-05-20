a7a6b56fa5f56f55b6f08cfb45be8d7fd183e294
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-20 14:28:31 -0300

    feat: ensure SurveyResult present correct result on success

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -9,7 +9,7 @@ type Props = {
 }
 
 const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
-  const [state] = useState({
+  const [state, setState] = useState({
     isLoading: false,
     error: '',
     surveyResult: null as LoadSurveyResult.Model
@@ -17,7 +17,12 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
 
   useEffect(() => {
     loadSurveyResult.load()
-      .then()
+      .then(surveyResult => {
+        setState(old => ({
+          ...old,
+          surveyResult
+        }))
+      })
       .catch()
   }, [])
 
@@ -28,25 +33,21 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
               {state.surveyResult &&
                 <>
                   <hgroup>
-                    <Calendar date={new Date()} className={Styles.calendarWrap} />
-                    <h2>Qual é seu framework web favorito?</h2>
+                    <Calendar date={state.surveyResult.date} className={Styles.calendarWrap} />
+                    <h2 data-testid="question">{state.surveyResult.question}</h2>
                   </hgroup>
-                  <FlipMove className={Styles.answerList}>
-                      <li>
-                          <img src='http://fordevs.herokuapp.com/static/img/logo-react.png'/>
-                          <span className={Styles.answer}>ReactJs</span>
-                          <span className={Styles.percent}>50%</span>
+                  <FlipMove data-testid="answers" className={Styles.answerList}>
+                    {state.surveyResult.answers.map(answer =>
+                      <li
+                        data-testid="answer-wrap"
+                        key={answer.answer}
+                        className={answer.isCurrentAccountAnswer ? Styles.active : ''}
+                      >
+                          {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer}/>}
+                          <span data-testid="answer" className={Styles.answer}>{answer.answer}</span>
+                          <span data-testid="percent" className={Styles.percent}>{answer.percent}%</span>
                       </li>
-                      <li>
-                          <img src='http://fordevs.herokuapp.com/static/img/logo-vue.png'/>
-                          <span className={Styles.answer}>VueJs</span>
-                          <span className={Styles.percent}>60%</span>
-                      </li>
-                      <li>
-                          <img src='http://fordevs.herokuapp.com/static/img/logo-angular.png'/>
-                          <span className={Styles.answer}>Angular</span>
-                          <span className={Styles.percent}>70%</span>
-                      </li>
+                    )}
                   </FlipMove>
                   <button>Voltar</button>
                 </>
