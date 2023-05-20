d587c8a099a5e81900010741b2eacd804d86203e
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-20 12:22:38 -0300

    test:ensure SurveyResult calls loadSurveyResult

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,20 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { SurveyResult } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
-import { mockAccountModel } from '@/domain/test'
+import { LoadSurveyResultSpy, mockAccountModel } from '@/domain/test'
 import { createMemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 
-const makeSut = (): void => {
+type SutTypes = {
+  loadSurveyResultSpy: LoadSurveyResultSpy
+}
+
+const makeSut = (): SutTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] })
+  const loadSurveyResultSpy = new LoadSurveyResultSpy()
   render(
       <ApiContext.Provider value={{ setCurrentAccount: jest.fn() , getCurrentAccount: () => mockAccountModel() }}>
         <Router location={history.location} navigator={history}>
-          < SurveyResult/>
+          < SurveyResult loadSurveyResult={loadSurveyResultSpy}/>
         </Router>
       </ApiContext.Provider>
   )
+
+  return {
+    loadSurveyResultSpy
+  }
 }
 
 describe('SurveyResultComponent', () => {
@@ -24,5 +33,12 @@ describe('SurveyResultComponent', () => {
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+    await waitFor(() => surveyResult)
+  })
+
+  test('Should call loadSurveyResult', async () => {
+    const { loadSurveyResultSpy } = makeSut()
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(loadSurveyResultSpy.callsCount).toBe(1)
   })
 })
