3057cf577a198b6a63d3119ebedee8d459765254
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-05-21 15:52:50 -0300

    test: ensure SurveyResult does not present Loading on active answer click

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -106,4 +106,12 @@ describe('SurveyResultComponent', () => {
     fireEvent.click(screen.queryByTestId('back-button'))
     expect(history.location.pathname).toBe('/')
   })
+
+  test('Should not present Loading on active answer click', async () => {
+    makeSut()
+    await waitFor(() => screen.getByTestId('survey-result'))
+    const answerWrap = screen.queryAllByTestId('answer-wrap')
+    fireEvent.click(answerWrap[0])
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+  })
 })
